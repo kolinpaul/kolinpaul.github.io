e3333ac8bba88ffd1a1db9d6ce837b9e1ba65ee0
kolinpaul <kolin.paul@gmail.com>
2025-07-23 10:54:50 +0000

    Deploying to gh-pages from @ kolinpaul/kolinpaul.github.io@2bc2dcba00bf4df4f388dc020ce698b50bbffedf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "IITD internal links for courses that I have taught/am teaching",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
